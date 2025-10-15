8a1ff1448d3461768fa8a46ed534dc9dad1d0c5e
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-10-15 20:20:55 +0900

    fix

--- myapp/resources/views/livewire/auth/register.blade.php
+++ myapp/resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,40 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Validation\Rules;
+use Livewire\Attributes\Layout;
+use Livewire\Volt\Component;
+
+new #[Layout('components.layouts.auth')] class extends Component {
+    public string $name = '';
+    public string $email = '';
+    public string $password = '';
+    public string $password_confirmation = '';
+
+    /**
+     * Handle an incoming registration request.
+     */
+    public function register(): void
+    {
+        $validated = $this->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        event(new Registered(($user = User::create($validated))));
+
+        Auth::login($user);
+
+        Session::regenerate();
+
+        $this->redirectIntended(route('dashboard', absolute: false), navigate: true);
+    }
+}; ?>
+
 <div class="flex flex-col gap-6">
     <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
 
